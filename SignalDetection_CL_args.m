97bbd84c9842a8d8fc2fb8b665add3dfd5243fb0
Matthew Kendzior <mkendzi2@iforge.ncsa.illinois.edu>
2021-01-14 21:29:58 -0600

    cleaning comments

--- SignalDetection_CL_args.m
+++ SignalDetection_CL_args.m
@@ -1,20 +1,23 @@
 %This script was first written by Dr. Liumdmila Mainzer and edited to allow for more general use by Matt Kendzior 
+%Callable on an input directory with per-base individual per base coverage files. At the moment this script only works with output from Sentieon's coverage metricss algorithm
+%These files look like:
+%Locus   Total_Depth     sample_Average_Depth    s_3595-ST-0709_Depth
+%chr1:866431     47      47.00   47
+%chr1:866432     48      48.00   48
+%...
 
-%function [] = SignalDetection_Mar23_CL_args(putatVarStart, putatVarStop)
+
 function [] = SignalDetection_Mar23_CL_args(putatVarStart, putatVarStop, filesDir, output)
 % set the coordinates for the putative variant we are trying to detect
 PutativeVariantStart=putatVarStart;
 PutativeVariantStop=putatVarStop;
 
 % find the coverage files
-%coveragefiles = dir('/projects/mgc/Project_2/cortex_var_runs/validating_coverage/covgmetrics_output/CNTNAP4_trios/*.covg_metrics_output');
-%coveragefiles = dir('/projects/mgc/Project_2/cortex_var_runs/validating_coverage/running_all_gene_loci_Dec2019/output/biobank/chr7_17930076_17930142_SNX13/*.covg_metrics_output');
 coveragefiles = dir(filesDir);
 NumFiles=size(coveragefiles);
 
 
 % create the output filer and print the headers
-%OutputFile = fopen('matlab_outputs/chr7_17930076_17930142_SNX13_biobank','w');
 OutputFile = fopen(output,'w');
 fprintf(OutputFile,'%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n','Sample','Chr','FirstChangePt','SecondChangePt','Residual','BeforeVarCoverageMean','BeforeVarCoverageStd',...
            'CoverageInVariantMean','CoverageInVariantStd','AfterVarCoverageMean','AfterVarCoverageStd','Zygocity');
@@ -50,8 +53,6 @@ for file = 1:NumFiles(1)
       fprintf(OutputFile,'%s\t%s\n',coveragefiles(file).name,'inappropriate number of change points found');
 
    % if both change points are inside the putative area, proceed with metrics; otherwise, print a message
-   %elseif (((str2num(FirstCoord{2})+TheChange(1)-1<PutativeVariantStart) & (str2num(FirstCoord{2})+TheChange(2)-1>PutativeVariantStop)) | (str2num(FirstCoord{2})+TheChange(1)-1>PutativeVariantStop)) | (str2num(FirstCoord{2})+TheChange(2)-1<PutativeVariantStart)
-%    elseif ( ((str2num(FirstCoord{2})+TheChange(1)-1>PutativeVariantStart) & (str2num(FirstCoord{2})+TheChange(2)-1<PutativeVariantStop)) & (((str2num(FirstCoord{2})+TheChange(2)) - (str2num(FirstCoord{2})+TheChange(1))) > ((PutativeVariantStop-PutativeVariantStart)/2)) )
      elseif ( ((str2num(FirstCoord{2})+TheChange(1)-1>PutativeVariantStart) & (str2num(FirstCoord{2})+TheChange(2)-1<PutativeVariantStop)) )
 
       %find mean and std coverage before, during and after the variant
@@ -95,5 +96,4 @@ end
 fclose(OutputFile);
 
 % print the total count of individuals with the variant to the Matlab console
-%fprintf('%s%d\n','CountIndividualsWithPutativeVariant=',CountIndividualsWithPutativeVariant);
 end
